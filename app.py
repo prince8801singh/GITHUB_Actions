9769696f182c534eb5cbd74f9192554bef52e652
prince8801singh <prince8801singh@gmail.com>
2026-06-20 21:42:39 +0530

    Added return None in line 11 for testing the linting fix

--- app.py
+++ app.py
@@ -3,11 +3,12 @@
 #Test
 from flask import Flask, render_template
 app = Flask(__name__)
-
+go
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+    return None
 
 
 @app.route('/health')
